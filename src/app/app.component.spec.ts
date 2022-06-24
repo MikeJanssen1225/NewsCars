c41f32b14638952bd4f9e32f1a33d6da5ef7a950
MikeJanssen1225 <89910147+MikeJanssen1225@users.noreply.github.com>
2022-06-24 15:38:25 +0200

    hoi

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NewsCars'`, () => {
+  it(`should have as title 'cars'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NewsCars');
+    expect(app.title).toEqual('cars');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('NewsCars app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cars app is running!');
   });
 });
